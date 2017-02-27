d771997d543ca8de27115bbbea0cadfef167a569
Mukesh B <mbhalani3@gmail.com>
2017-02-27 09:16:43 -0800

    initial commit

---
+++ only-unique.js
@@ -0,0 +1,13 @@
+/*
+Only Unique
+Given a string, remove any characters that are not unique from the string.
+
+          Examples             Input Output
+string:   "abccdefe"            "abdf"
+string:   "hello there"         "h tr"
+string:   "xyz"                 "xyz"
+string:   "iiii"                ""
+*/
+
+function onlyUnique(str) {
+}
