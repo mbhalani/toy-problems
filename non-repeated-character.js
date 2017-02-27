0818cdc5fefa32ac82af6710d3a353630139f54d
Mukesh B <mbhalani3@gmail.com>
2017-02-27 09:09:17 -0800

    complete using indexOf

---
+++ non-repeated-character.js
@@ -0,0 +1,26 @@
+/*
+Non-repeated Character
+Given an arbitrary input string, return the first non-repeating character. For strings with all repeats, return 'sorry'.
+
+          Examples             Input Output
+string:   "ABCDBIRDUP"          "A"
+string:   "XXXXXXX"             "sorry"
+string:   "ALAMABA"             "L"
+string:   "BABA"                "sorry"
+*/
+function firstNonRepeatedCharacter (string) {
+  for (let i = 0; i < string.length; i++) {
+    let result = string.charAt(i);
+
+    if (string.indexOf(result) === i && string.indexOf(result, i + 1) === -1) {
+      return result;
+    }
+  }
+
+  return 'sorry';
+}
+
+console.log(firstNonRepeatedCharacter('ABCDBIRDUP'));
+console.log(firstNonRepeatedCharacter('XXX'));
+console.log(firstNonRepeatedCharacter('ALABAMA'));
+console.log(firstNonRepeatedCharacter('BABA'));
