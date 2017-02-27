af95e10facd8294c0b316cfaec68566544ed3201
Mukesh B <mbhalani3@gmail.com>
2017-02-27 10:16:28 -0800

    complete

--- sum-array.js
+++ sum-array.js
@@ -10,4 +10,22 @@ array:   [1,2,3,-6,4,5,6]      15
 */
 
 function sumArray(array) {
+  let maxSum = Number.NEGATIVE_INFINITY;
+  let currentSum = 0;
+
+  array.forEach((item) => {
+    currentSum += item;
+    maxSum = Math.max(maxSum, currentSum);
+
+    if (currentSum < 0) {
+      currentSum = 0;
+    }
+  });
+
+  return maxSum;
 }
+
+console.log(sumArray([1,2,3]));
+console.log(sumArray([4,-1,5]));
+console.log(sumArray([10,-11,11]));
+console.log(sumArray([1,2,3,-6,4,5,6]));
