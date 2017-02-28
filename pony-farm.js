a0d8b3ea4d3fc18843f4f013e1110fa32eb970fa
Mukesh B <mbhalani3@gmail.com>
2017-02-28 13:43:54 -0800

    complete

--- pony-farm.js
+++ pony-farm.js
@@ -10,8 +10,34 @@ owner should avoid feeding his/her ponies.
 
 NOTE: You are required to use at least one map and one filter. Pros can
 use reduce to make their code even more functional-style.
+
+Examples
+Input
+ponies:
+[ { "id": 427, "name": "Firefly", "allergies": [ "gluten", "peanut" ], "email": "cindy@ponymail.com" }, { "id": 23, "name": "Black Lightning", "allergies": [ "soy", "peanut" ], "email": "sandy@hotmail.com" }, { "id": 458, "name": "Picadilly", "allergies": [ "corn", "gluten" ], "email": "cindy@ponymail.com" }, { "id": 142, "name": "Brad", "allergies": [ "gluten", "chicken" ], "email": "sandy@hotmail.com" }, { "id": 184, "name": "Cahoot", "allergies": [ "soy", "peanut", "gluten" ], "email": "jimmy@ponymail.com" } ]
+ownerEmail:
+"sandy@hotmail.com"
+
+Output
+[ "chicken", "gluten", "peanut", "soy" ]
 */
 
-function getPonyAllergies(ponies, ownerEmail) {
+var ponies =
+[ { "id": 427, "name": "Firefly", "allergies": [ "gluten", "peanut" ], "email": "cindy@ponymail.com" }, { "id": 23, "name": "Black Lightning", "allergies": [ "soy", "peanut" ], "email": "sandy@hotmail.com" }, { "id": 458, "name": "Picadilly", "allergies": [ "corn", "gluten" ], "email": "cindy@ponymail.com" }, { "id": 142, "name": "Brad", "allergies": [ "gluten", "chicken" ], "email": "sandy@hotmail.com" }, { "id": 184, "name": "Cahoot", "allergies": [ "soy", "peanut", "gluten" ], "email": "jimmy@ponymail.com" } ];
 
+var ownerEmail = 'sandy@hotmail.com';
+
+function getPonyAllergies(ponies, ownerEmail) {
+  return ponies.reduce((items, pony) => {
+    if (ownerEmail === pony.email) {
+      pony.allergies.forEach((allergy) => {
+        if (items.indexOf(allergy) === -1) {
+          items.push(allergy);
+        }
+      });
+    }
+    return items;
+  }, []).sort();
 }
+
+console.log(getPonyAllergies(ponies, ownerEmail));
