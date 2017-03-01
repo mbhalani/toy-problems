79dcf026357fde8c2e2331e0fa7072fe154946d2
Mukesh B <mbhalani3@gmail.com>
2017-03-01 08:52:11 -0800

    complete

--- words-within-words.js
+++ words-within-words.js
@@ -1,6 +1,8 @@
 /*
 Words within Words
-Given an array of unique words, find the word that contains the greatest number of other words. A word must be at least two letters long.
+Given an array of unique words, find the word that
+contains the greatest number of other words. A word must
+be at least two letters long.
 
 Examples
 Input Output
@@ -9,4 +11,26 @@ wordList:
 */
 
 function nestedWordCount(wordList) {
+  let maxCount = 0;
+  let result;
+
+  wordList.forEach( (firstWord) => {
+    let count = 0;
+
+    wordList.forEach( (secondWord) => {
+      if (firstWord.includes(secondWord)) {
+        count++;
+      }
+    });
+
+    if (count > maxCount) {
+      maxCount = count;
+      result = firstWord;
+    }
+  });
+
+  return result || '';
 }
+
+let words = [ "gray", "grays", "ray", "rays", "strays" ] ;
+console.log(nestedWordCount(words));
