c59cf972be7cfb336df142c71eaa24b01cded771
Mukesh B <mbhalani3@gmail.com>
2017-03-12 12:01:39 -0400

    complete

--- hash-table.js
+++ hash-table.js
@@ -11,17 +11,42 @@ var makeHashTable = function(){
   var storageLimit = 1000;
 
   table.insert = function(key, value){
+    // get unique index from hash function using key
     var index = getIndexBelowMaxForKey(key, storageLimit);
+    //storage[index] is a bucket
     storage[index] = storage[index] || [];
     //YOUR CODE HERE
+    // remove existing key
+    this.remove(key);
+    // push key-value pair to bucket
+    storage[index].push([key, value]);
   };
 
   table.retrieve = function(key){
     //YOUR CODE HERE
+    var index = getIndexBelowMaxForKey(key, storageLimit);
+    if (storage[index]) {
+      for (let i; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          return storage[index][i][1];
+        }
+      }
+    }
   };
 
   table.remove = function(key){
     //YOUR CODE HERE
+    // get unique index from hash function using key
+    var index = getIndexBelowMaxForKey(key, storageLimit);
+
+    // if bucket exists iterate over bucket
+    if (storage[index]) {
+      for (let i = 0; i < storage[index].length; i++) {
+        if (storage[index][i][0] === key) {
+          storage[index].splice(i, 1);
+        }
+      }
+    }
   };
   return table;
 };
@@ -35,3 +60,5 @@ var getIndexBelowMaxForKey = function(str, max){
  }
  return hash % max;
 };
+
+
