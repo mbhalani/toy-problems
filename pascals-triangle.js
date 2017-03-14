2d2c000968aae682e0238b44e0fe73a612dcfa69
Mukesh B <mbhalani3@gmail.com>
2017-03-14 13:42:35 -0400

    initial commit

---
+++ pascals-triangle.js
@@ -0,0 +1,19 @@
+/*
+Pascal's Triangle
+
+Pascal's Triangle
+
+Wikipedia article on Pascal's Triangle: http://en.wikipedia.org/wiki/Pascal's_triangle
+
+Write a function that, given a depth (n), returns a single-dimensional array
+representing Pascal's Triangle to the n-th level.
+
+For example:
+
+pascalsTriangle(4) == [1,1,1,1,2,1,1,3,3,1]
+*/
+
+function pascalsTriangle(n) {
+  //return a flat array representing the values of Pascal's Triangle to the n-th level
+
+}
